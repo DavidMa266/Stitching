c73d1991c5389a75772da22cfe25f932adac9482
David Ma <dsm266@cornell.edu>
2015-11-15 01:15:58 -0500

    Sobel updated. Lines too thick. Consider looking into non_maximum_suppression or atan2

--- src/main/Sobel.java
+++ src/main/Sobel.java
@@ -5,7 +5,7 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
-import java.util.HashMap;
+import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 
@@ -15,34 +15,45 @@ public class Sobel {
 	
 	//Image map represents the RGB colors of the original image
 	private RGB[][] image_map;
-	private double[][] blur_map;
+	private int[][] blur_map;
 	
-	private double stdev = 1.4;
-	private double g_constant = 1/ (2 * Math.PI * stdev * stdev);
+	private double stdev;
 	
-	//These dims must be odd > 1
-	private int blur_dim = 5;
-	private int sobel_dim = 3;
+	//These dims must be odd and > 1
+	private int blur_dim;
+	private final int sobel_dim = 3;
 	
 	double[] G_matrix;
 	
 	//These variables are used for the Sobel edge generator
-	private double[] Sx = new double[]{-1,  0,  1, -2, 0, 2, -1, 0, 1};
-	private double[] Sy = new double[]{-1, -2, -1,  0, 0,  0, 1, 2, 1};
-	private double[][] S_result;
+	private double[] Sx = new double[]{1, 2,  1, 0, 0, 0, -1, -2, -1};
+	private double[] Sy = new double[]{1, 0, -1,  2, 0,  -2, 1, 0, -1};
+	private int[][] S_result;
+	private boolean[][] S_threshold;
+	private double[][] S_orientation;
+
 	
 	private int width;
 	private int height;
 	
+	public Sobel(double stdev, int blur_dim){
+		this.stdev = stdev;
+
+		this.blur_dim = blur_dim;
+		if(blur_dim %2 == 0){
+			blur_dim ++;
+		}
+	}
 	
-	public void test(String filename){
+	public void test(String filename, int weak, int strong){
 		long startTime = System.nanoTime();
 		BufferedImage im = load_image(filename);
 		if(im == null)
 			return;
 		gaussian_blur();
 		sobel();
-		set_image();
+		non_maximum_suppression(weak, strong);
+		set_image();		
 		long endTime = System.nanoTime();
 		System.out.println("Took "+(endTime - startTime) + " ns"); 
 	}
@@ -51,14 +62,14 @@ public class Sobel {
 	 * This method applies a Gaussian blur to the original image
 	 */
 	private void gaussian_blur(){
-	
+
+		double g_constant = 1/ (2 * Math.PI * stdev * stdev);
 		G_matrix = new double[blur_dim * blur_dim];
 		int index = 0;
 		//We construct the Gauss matrix
 		for(int k = -blur_dim/2; k <= blur_dim/2; k++){
 			for(int l = -blur_dim/2; l <= blur_dim/2; l++){
 				double gauss = g_constant * Math.pow(Math.E, -1 * ((Math.pow(k, 2) + Math.pow(l, 2))/(2 * stdev * stdev)));
-				
 				G_matrix[index] = gauss;
 				index++;
 			}
@@ -68,12 +79,12 @@ public class Sobel {
 		for(int i = blur_dim/2; i < width-blur_dim/2; i++){
 			for(int j = blur_dim/2; j < height-blur_dim/2; j++){
 				//Now we construct the target matrix		
-				double[] target_matrix = new double[blur_dim * blur_dim];
+				int[] target_matrix = new int[blur_dim * blur_dim];
 
 				index = 0;
 				for(int k = i-blur_dim/2; k <= i+blur_dim/2; k++){
 					for(int l = j-blur_dim/2; l <= j+blur_dim/2; l++){
-						target_matrix[index]= image_map[k][l].luminance();
+						target_matrix[index]= (int) image_map[k][l].luminance();
 						index++;
 					}
 				}
@@ -82,7 +93,7 @@ public class Sobel {
 				//Image convolution
 				for(int k = 0; k < blur_dim * blur_dim; k++)
 					res += G_matrix[k] * target_matrix[k];
-				blur_map[i][j] = res;
+				blur_map[i][j] = (int) res;
 				
 			}
 		}
@@ -101,7 +112,7 @@ public class Sobel {
 					for(int l = j-blur_dim/2; l <= j+blur_dim/2; l++){
 						sum += image_map[k][l].luminance();
 					}
-					blur_map[i][j] = sum/(blur_dim * blur_dim);
+					blur_map[i][j] = (int) (sum/(blur_dim * blur_dim));
 				}
 			}
 		}
@@ -117,7 +128,7 @@ public class Sobel {
 		
 		for(int i = blur_dim/2 + sobel_dim/2; i < width-blur_dim/2-sobel_dim/2; i++){
 			for(int j = blur_dim/2 + sobel_dim/2; j < height-blur_dim/2-sobel_dim/2; j++){
-				double[] target_matrix = new double[sobel_dim * sobel_dim];
+				int[] target_matrix = new int[sobel_dim * sobel_dim];
 				int index = 0;
 				//We construct the target matrix
 				for(int k = i-sobel_dim/2; k <= i+sobel_dim/2; k++){
@@ -126,30 +137,197 @@ public class Sobel {
 						index++;
 					}
 				}
-				double x_res = 0;
-				double y_res = 0;
+				int x_res = 0;
+				int y_res = 0;
 				
 				//This is essentially image convolution
 				for(int k = 0; k < sobel_dim * sobel_dim; k++){
 					x_res += Sx[k] * target_matrix[k];
 					y_res += Sy[k] * target_matrix[k];
 				}
-
+				//x_res and y_res both represent the change in luminosities from the x and y directions respectively
+				//Thus the total overall change is found by computing the magnitude
+				//Since we also have the relative changes, by using the atan2 function, we can find the angle and thus direction of the edge
 				double magnitude = magnitude(x_res, y_res);
-				
-				S_result[i][j] = magnitude;
+
+				S_result[i][j] = (int) magnitude;
+				//Conversion to degrees
+				S_orientation[i][j] = Math.atan2(y_res, x_res) * 180 / Math.PI;
 			}
 		}
 	}
 	
+	
+	private void non_maximum_suppression(int weak, int strong){
+		for(int i = 1; i < width-1; i++){
+			for(int j = 1; j < height-1; j++){
+				
+				blur_map[i][j] = 0;
+				int ne = S_result[i-1][j+1];
+				int n = S_result[i][j+1];
+				int nw = S_result[i+1][j+1];
+				
+				int e = S_result[i-1][j];
+				int c = S_result[i][j];
+				int w = S_result[i+1][j];
+				
+				int se = S_result[i-1][j-1];
+				int s = S_result[i][j-1];
+				int sw = S_result[i+1][j-1];
+				
+				
+				int f_target = 0;
+				int f_next = 0;
+				int b_target = 0;
+				int b_next = 0;
+				double degrees = S_orientation[i][j];
+				
+				int direction = (int) (degrees % 45);
+				switch (direction){
+				case 0:
+					f_target = e;
+					f_next = ne;
+					b_target = w;
+					b_next = sw;
+				case 1:
+					f_target = ne;
+					f_next = n;
+					b_target = sw;
+					b_next = s;
+				case 2:
+					f_target = n;
+					f_next = nw;
+					b_target = s;
+					b_next = se;
+				case 3:
+					f_target = nw;
+					f_next = w;
+					b_target = se;
+					b_next = e;
+				case 4:
+					f_target = w;
+					f_next = sw;
+					b_target = e;
+					b_next = ne;
+				case 5:
+					f_target = sw;
+					f_next = s;
+					b_target = ne;
+					b_next = n;
+				case 6:
+					f_target = s;
+					f_next = se;
+					b_target = n;
+					b_next = nw;
+				case 7:
+					f_target = se;
+					f_next = e;
+					b_target = nw;
+					b_next = w;
+				}
+				
+				double f_res = f_target;
+				double b_res = b_target;
+				double overflow = degrees %45;
+				if(overflow > 22){
+					f_res = f_next;
+					b_res = b_next;
+				}
+				
+				
+			//	double overflow = ((double)(degrees %45))/45;
+				
+			//	double f_res = f_target * (1-overflow) + f_next * overflow;
+			//	double b_res = b_target * (1-overflow) + b_next * overflow;
+				
+				//Preserve values that are greater than the gradients
+				if(c > f_res && c > b_res){
+					
+					
+					
+				}
+				else{
+					S_threshold[i][j] = true;
+				}
+				
+			}
+		}
+		for(int i = 0; i < width; i++){
+			for(int j = 0; j < height; j++){
+				if(S_threshold[i][j]){
+					S_result[i][j] = 0;
+					S_threshold[i][j] = false;
+				}else if(S_result[i][j] > strong){
+					//STRONG edge, 
+					S_threshold[i][j] = true;
+				}//NO edge
+				else if(S_result[i][j] < weak){
+					S_result[i][j] = 0;
+				}
+			}
+		}
+		for(int i = 0; i < width; i++){
+			for(int j = 0; j < height; j++){
+				//WEAK or NONE
+				if(!S_threshold[i][j]){
+					//WEAK 
+					if(S_result[i][j] != 0){
+						//Has strong edge, preserve
+						if(strong_edge(i,j))
+							S_result[i][j] = 120;
+						else
+							S_result[i][j] = 0;
+					}
+					//NONE is ignored
+				}
+				//STRONG
+				else
+					S_result[i][j] = 255;
+				S_threshold[i][j] = false;
+			}
+		}
+	}
+	
+	private boolean strong_edge(int i, int j){
+		return S_threshold[i][j-1] ||  S_threshold[i][j+1] ||
+				 S_threshold[i-1][j-1] ||  S_threshold[i-1][j] ||  S_threshold[i-1][j+1] ||
+				 S_threshold[i+1][j-1] ||  S_threshold[i+1][j] ||  S_threshold[i+1][j+1];  
+	}
+	
+	private void hough(int max_radius){
+		for(int i = 0; i < width; i++){
+			for(int j = 0; j < height; j++){
+				if(S_result[i][j] > 0){
+					
+					for(int r = 1; r < max_radius; r++){
+					
+						for(int theta = 0; theta < 360; theta++){
+							double angle = theta / 180 * Math.PI;
+							double c_i = Math.cos(angle);
+							double c_j = Math.sin(angle);
+							S_threshold[i][j] = true;
+						}
+					
+					}
+					
+					
+					
+				}
+			}
+		}
+		
+	}
+	
+	
 	/**
-	 * Sets the image using the results generated from G_result
+	 * Sets the image using the results generated from S_result
 	 */
 	private void set_image() {
+
 		BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 		for(int x = 0; x < width; x++){
 			for(int y = 0; y < height; y++){
-				int color = (int)S_result[x][y];
+				int color = S_result[x][y];
 				color = color << 16 | color << 8 | color;
 				//Get each pixel, convert to color coordinates, and set the x, y coordinate on the image to that color
 				img.setRGB(x, y, color);
@@ -182,9 +360,10 @@ public class Sobel {
 			width = im.getWidth();
 			height = im.getHeight();
 			image_map = new RGB[width][height];
-			blur_map = new double[width][height];
-			S_result = new double[width][height];	
-			
+			blur_map = new int[width][height];
+			S_result = new int[width][height];	
+			S_threshold = new boolean[width][height];
+			S_orientation = new double[width][height];
 			//Loading in the initial file
 			for(int i = 0; i < width; i++){
 				for(int j = 0; j < height; j++){
